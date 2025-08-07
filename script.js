341ffb2f9aff37b00d68ad0ef5615f57f4c5d781
Aadya <102467098+aadya-khanna@users.noreply.github.com>
2025-08-06 21:14:36 -0400

    added files again

--- script.js
+++ script.js
@@ -1,24 +1,8 @@
-var c = document.getElementById("gameCanvas");
-var ctx = c.getContext("2d");
+const c = document.getElementById("gameCanvas");
+const ctx = c.getContext("2d");
 
-var img = new Image();
-img.src = 'assets/Scene Overview.png';
+const tileset = new Image();
+tileset.src = "TX Tileset Grass.png";
 
-let cameraX= 0;
-let cameraY = 0; 
 
-ctx.drawImage(
-    map,            //
-    cameraX,        // crop start x in image
-    cameraY,        // crop start y in image
-    canvas.width,   // crop width
-    canvas.height,  // crop height
-    0, 0,           // draw position on canvas
-    canvas.width,
-    canvas.height
-  );
-  
-
-img.onload = () => {
-    ctx.drawImage(img, 0, 0, c.width, c.height);
-}
+const tileSize = 16; 
